1564fb3584d868a75eab93f08b05eeddcb7622b1
brutchley <bhutchison0901@yahoo.com>
2023-10-07 12:58:35 -0500

    added js variables for timer, as well as first half of my timer function (without 'time's up' feature) git push origin main

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsleft = 60
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsleft--;
+        timeEl.textContent = secondsleft = " seconds left!"
+        if (secondsleft === 0) {
+            clearInterval(timerInterval);
+            
+            sendMessage();
+        }
+    }, 1000);
+}
